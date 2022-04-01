6a3b010b8e8b99331bd81731aec116600f687652
KERROUMI hajar <kerroumihajar98@gmail.com>
2022-04-01 14:57:50 +0000

    Initial commit

---
+++ src/com/ensa/gi4/datatabase/Dbclass.java
@@ -0,0 +1,12 @@
+package com.ensa.gi4.datatabase;
+
+import com.ensa.gi4.modele.Chaise;
+import com.ensa.gi4.modele.Livre;
+import com.ensa.gi4.modele.Materiel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dbclass {
+    public List<Materiel> material = new ArrayList<>();
+}
---
+++ src/com/ensa/gi4/service/api/GestionMaterielService.java
@@ -0,0 +1,20 @@
+package com.ensa.gi4.service.api;
+
+import com.ensa.gi4.modele.Materiel;
+
+import java.util.List;
+
+public interface GestionMaterielService {
+    void init();
+    public void  listerMateriel();
+  public void addMaterial( );
+   public void deletMaterial();
+//   public  void upDate();
+   public void search();
+
+    void upDate();
+//
+//    public void deleteMaterial(int theId);
+
+
+}
