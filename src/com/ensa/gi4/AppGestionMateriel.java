58b8986bdf8f7306cada318ff9d522ee59514e16
KERROUMI hajar <82770077+KRhajar@users.noreply.github.com>
2022-04-16 13:13:02 +0000

    Update AppGestionMateriel.java

--- src/com/ensa/gi4/AppGestionMateriel.java
+++ src/com/ensa/gi4/AppGestionMateriel.java
@@ -5,11 +5,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SuppressWarnings("all")
+@ComponentScan(basePackages = {"com.ensa.gi4"})
 public class AppGestionMateriel {
     private static final ApplicationContext APPLICATION_CONTEXT;
 
     static { // bloc static pour initilialisation
-        APPLICATION_CONTEXT = new ClassPathXmlApplicationContext("/beans/app-context.xml");
+       // APPLICATION_CONTEXT = new ClassPathXmlApplicationContext("/beans/app-context.xml");
+              
+       APPLICATION_CONTEXT = new AnnotationConfigApplicationContext(AppGestionMateriel.class);
     }
 
     public static  void main(String[] args) throws InterruptedException {
